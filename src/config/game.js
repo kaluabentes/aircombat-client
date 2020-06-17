89fd46e4191478440cde76c69592855492ff5078
Kaluã Bentes <kaluanbentes@gmail.com>
2020-06-17 01:22:02 -0300

    refactor(cloud): change size

--- src/config/game.js
+++ src/config/game.js
@@ -1,7 +1,7 @@
 export const MAX_VIEWPORT_WIDTH = 1280;
 export const OUTSIDE_COLOR = 0x00000;
 
-export const WORLD_HEIGHT = 5000;
-export const WORLD_WIDTH = 5000;
+export const WORLD_HEIGHT = 10000;
+export const WORLD_WIDTH = 10000;
 
 export const GROUND_COLOR = 0x89c0fb;
