b411fbc5f7dc3295e4e53f831667c17a827c2a2c
Kaluã Bentes <kaluanbentes@gmail.com>
2020-06-25 23:27:57 -0300

    refactor(hud): take jet fresh instance

--- src/scenes/HudScene.js
+++ src/scenes/HudScene.js
@@ -1,7 +1,6 @@
 import Phaser from "phaser";
 
 import PlayerHpBar from "../objects/PlayerHpBar";
-import { MAX_HP } from "../config/game";
 
 export default class HudScene extends Phaser.Scene {
   constructor() {
@@ -13,13 +12,13 @@ export default class HudScene extends Phaser.Scene {
   create(data) {
     const { player } = data;
     const { jet } = player;
-    this.jet = jet;
+    this.player = player;
 
     this.scene.sendToBack("GameScene");
     this.playerHpBar = new PlayerHpBar(this, 180, 90, jet.hp, player.name);
   }
 
   update() {
-    this.playerHpBar.update(this.jet);
+    this.playerHpBar.update(this.player.jet);
   }
 }
